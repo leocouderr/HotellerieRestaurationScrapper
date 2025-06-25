fc454a45aae175a011a3e487ce523d9cd8bc7dd0
leocouderr <107613264+leocouderr@users.noreply.github.com>
2025-06-25 17:59:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import datetime
 import json
+from selenium.webdriver.chrome.options import Options
 import os
 
 
